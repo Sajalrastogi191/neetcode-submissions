f9ab20d29cf4cc37e11fe240cb00518adf931668
Sajal Rastogi <rastogisajal1915@gmail.com>
2026-06-27 21:11:38 +0530

    Add: n-queens-ii - submission-0

---
+++ Data Structures & Algorithms/n-queens-ii/submission-0.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    bool isvalid(int i,int j,vector<vector<int>>& board,int n) {
+        //row 
+        for(int l = j-1;l>=0;l--) {
+            if(board[i][l]) return false;
+        }
+        //col
+        for(int l = i-1;l>=0;l--) {
+            if(board[l][j]) return false;
+        }
+        //left diagonal
+        for(int l = i-1,m=j-1;l>=0 && m >= 0;l--,m--) {
+            if(board[l][m]) return false;
+        }
+        //right diagonal
+        for(int l = i-1,m=j+1;l>=0 && m < n;l--,m++) {
+            if(board[l][m]) return false;
+        }
+        return true;
+    }
+    void helper(int i,int n,set<vector<string>>& ans,vector<vector<int>>& board) {
+        if(i == n) {
+            vector<string> sol;
+            for(int i = 0;i<n;i++) {
+                string tmp;
+                for(int j = 0;j<n;j++) {
+                    if(board[i][j]) {
+                        tmp.push_back('Q');
+                    } else {
+                        tmp.push_back('.');
+                    }
+                }
+                sol.push_back(tmp);
+            }
+            ans.insert(sol);
+            return;
+        }
+
+        for(int j = 0;j<n;j++) {
+            if(isvalid(i,j,board,n)) {
+                board[i][j] = 1;
+                helper(i+1,n,ans,board);
+                board[i][j] = 0;
+            }
+        }
+
+        return;
+    }
+    int totalNQueens(int n) {
+        vector<vector<int>> board(n,vector<int>(n,0));
+        set<vector<string>> ans;
+        vector<string> sol;
+        helper(0,n,ans,board);
+        return ans.size();
+    }
+};
