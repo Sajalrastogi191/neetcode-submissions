0a7c10b66c191e1f054d416d09a703bd3e775b7e
Sajal Rastogi <rastogisajal1915@gmail.com>
2026-06-28 16:57:21 +0530

    Add: network-delay-time - submission-0

---
+++ Data Structures & Algorithms/network-delay-time/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto &it:times) {
+            int u = it[0],v = it[1], t = it[2];
+            adj[u-1].push_back({v-1,t});
+        }
+        queue<pair<int,int>> q;
+        q.push({k-1,0});
+        vector<int> dist(n,INT_MAX);
+        dist[k-1] = 0;
+        while(!q.empty()) {
+            auto [u,t] = q.front();
+            q.pop();
+
+            for(auto &[v,wt]:adj[u]) {
+                if(dist[u] + wt < dist[v]) {
+                    dist[v] = dist[u] + wt;
+                    q.push({v,dist[v]});
+                }
+            }
+        }
+        int ans = 0;
+        for(int i = 0;i<n;i++) {
+            ans = max(ans,dist[i]);
+        }
+        return (ans==INT_MAX)? -1:ans;
+    }
+};
